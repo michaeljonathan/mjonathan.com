a46a66181f3602cba37297ebfb041c83ab439a0a
MJ <mjonathan1113@gmail.com>
2015-08-06 23:50:20 +1000

    Added basic ui-router navigation

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,39 @@
-angular.module('mainApp', []);
+'use strict';
+
+var mjApp = angular.module('mjApp', ['ui.router']);
+
+mjApp.config(function($stateProvider, $urlRouterProvider) {
+
+	// Route: default
+	$urlRouterProvider.otherwise('/');
+
+	// Route: specifics
+	$stateProvider
+
+		// Home
+		.state('home', {
+			url: '/',
+			templateUrl: 'templates/home.html'
+		})
+
+		// Projects
+		.state('projects', {
+			abstract: true,
+			url: '/projects',
+			template: '<div ui-view></div>'
+		})
+		.state('projects.list', {
+			url: '',
+			templateUrl: 'templates/projects.list.html'
+		})
+		.state('projects.detail', {
+			url: '/{projectId:[A-Za-z0-9\-]{1,100}}',
+			templateUrl: 'templates/projects.detail.html'
+		})
+
+		// About
+		.state('about', {
+			url: '/about',
+			templateUrl: 'templates/about.html'
+		});
+});
