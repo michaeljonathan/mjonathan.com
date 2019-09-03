b002c808bde71cd2ca5149c18caea97a9e7524e2
MJ <>
2019-09-03 21:35:49 +1000

    Update path

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
   title: 'Michael Jonathan', // Title for your website.
   tagline: 'mjonathan.com',
-  url: 'https://www.mjonathan.com', // Your website URL
+  url: 'https://michaeljonathan.github.io', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
